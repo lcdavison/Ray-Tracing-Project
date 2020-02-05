7cd3e0ecba0ef8b72345ee13770f18b6a0546703
lcdavison <luke.davison@talktalk.net>
2020-02-05 15:47:16 +0000

    Updated HitResult stored data

--- include/geometry/hitresult.h
+++ include/geometry/hitresult.h
@@ -1,8 +1,18 @@
 #ifndef HITRESULT_H
 #define HITRESULT_H
 
+#include "maths/maths.h"
+
+/*
+ *	HitResult contains all information about the intersection of
+ *	a ray and the geometry in a scene.
+ * */
 struct HitResult 
 {
-	bool hit = false;	
+	bool 		m_hit = false;		//	Has ray hit something
+	double 		m_distance = 0.0;	//	Distance of hit point
+	Point3		m_hitpoint;		//	Hit point in world coordinates
+	//	Ray
+	//	Material
 };
 #endif
