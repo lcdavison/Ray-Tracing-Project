7f4c1b67bc8bc91ad583b5a632168bdfda6ed763
lcdavison <luke.davison@talktalk.net>
2020-02-08 21:26:59 +0000

    Added intersection computation

--- source/geometry/plane.cpp
+++ source/geometry/plane.cpp
@@ -1,10 +1,18 @@
 #include "geometry/plane.h"
 
-Plane::Plane ( )
-{
-}
+Plane::Plane ( ) : m_point ( 0.0, 0.0, 0.0 ), m_normal ( 0.0, 1.0, 0.0 ) { }
+
+Plane::Plane ( const Point3& p_point, const Vector3& p_normal ) : m_point ( p_point ), m_normal ( p_normal ) { }
 
 bool Plane::rayhit ( const Ray& p_ray, HitResult& p_hitresult )
 {
-	return false;
+	Vector3 plane_to_origin = p_ray.get_origin ( ) - this->m_point;
+
+	double distance = ( -1.0f * dot ( plane_to_origin, this->m_normal ) ) / dot ( p_ray.get_direction ( ), this->m_normal );
+
+	if ( distance > 0.0 )
+	{
+		p_hitresult.m_hit = true;
+		p_hitresult.m_distance = distance;
+	}
 }
