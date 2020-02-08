4567432c0c6a14939df54a29f5491f1dcc40e978
lcdavison <luke.davison@talktalk.net>
2020-02-08 22:10:55 +0000

    Implemented pinhole camera

---
+++ source/camera/pinhole_camera.cpp
@@ -0,0 +1,31 @@
+#include "camera/pinhole_camera.h"
+
+PinholeCamera::PinholeCamera ( ) { }
+
+PinholeCamera::PinholeCamera ( const Point3& p_position, double p_vpdist ) : m_viewplane_dist ( p_vpdist )
+{
+	m_position = p_position;
+}
+
+void PinholeCamera::construct_basis ( const Point3& p_target, const Vector3& p_world_up )
+{
+	Vector3 target_to_cam = m_position - p_target;
+	target_to_cam.normalize ( );
+
+	m_basis [ 0 ] = cross ( p_world_up, target_to_cam );		//	x-axis
+	m_basis [ 1 ] = cross ( target_to_cam, m_basis [ 0 ] );		//	y-axis
+	m_basis [ 2 ] = target_to_cam;					//	z-axis
+}
+
+Ray PinholeCamera::construct_ray ( double p_pixelx, double p_pixely )
+{
+	//	Remap coordinates
+	double x = p_pixelx - ( m_viewplane_width * 0.5 );	
+	double y = p_pixely - ( m_viewplane_height * 0.5 );
+
+	Vector3 ray_dir ( x - m_position.x, y - m_position.y, m_viewplane_dist );
+	ray_dir = ray_dir.x * m_basis [ 0 ] + ray_dir.y * m_basis [ 1 ] + ray_dir.z * m_basis [ 2 ];	//	Rotate ray into world space
+	ray_dir.normalize ( );
+
+	return Ray ( m_position, ray_dir );
+}
