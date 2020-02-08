24b5ad5897ef29d10865dcb7b8e480a7d5f7414a
lcdavison <luke.davison@talktalk.net>
2020-02-08 21:26:23 +0000

    Corrected indentation

--- include/geometry/plane.h
+++ include/geometry/plane.h
@@ -7,12 +7,13 @@ class Plane : public IGeometry
 {
 	public:
 		Plane ( );
+		Plane ( const Point3&, const Vector3& );
 
-		bool rayhit ( const Ray&, HitResult& );
+		bool 		rayhit ( const Ray&, HitResult& );
 
 	private:
 
-		Point3 m_point;
-		Vector3 m_normal;
+		Point3 		m_point;
+		Vector3 	m_normal;
 };
 #endif
