6b0c549e00dd95ed650ca896576c6b3bb662fa2b
lcdavison <luke.davison@talktalk.net>
2020-02-11 19:05:34 +0000

    Simplified intersection distance equation

--- source/geometry/plane.cpp
+++ source/geometry/plane.cpp
@@ -1,14 +1,16 @@
 #include "geometry/plane.h"
 
+#include <iostream>
+
 Plane::Plane ( ) : m_point ( 0.0, 0.0, 0.0 ), m_normal ( 0.0, 1.0, 0.0 ) { }
 
 Plane::Plane ( const Point3& p_point, const Vector3& p_normal ) : m_point ( p_point ), m_normal ( p_normal ) { }
 
 bool Plane::rayhit ( const Ray& p_ray, HitResult& p_hitresult )
 {
-	Vector3 plane_to_origin = p_ray.get_origin ( ) - this->m_point;
+	Vector3 plane_to_origin = this->m_point - p_ray.get_origin ( );
 
-	double distance = ( -1.0f * dot ( plane_to_origin, this->m_normal ) ) / dot ( p_ray.get_direction ( ), this->m_normal );
+	double distance = dot ( plane_to_origin, this->m_normal ) / dot ( p_ray.get_direction ( ), this->m_normal );
 
 	if ( distance > 0.0 )
 	{
