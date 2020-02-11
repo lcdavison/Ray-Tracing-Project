c1da50c74bd5d39090c2fdd0a3dd66ebecdb3620
lcdavison <luke.davison@talktalk.net>
2020-02-11 19:06:52 +0000

    Added plane and basic ray tracing algorithm

--- source/scene/scene.cpp
+++ source/scene/scene.cpp
@@ -23,6 +23,8 @@ Scene::~Scene ( )
 void Scene::build_scene ( )
 {
 	//	Create geometry, lights, materials, etc..
+	Plane* plane = new Plane ( Point3 ( -10.0, 0.0, 0.0 ), Vector3 ( 1.0, 0.0, 0.0 ) );
+	m_geometry.push_back ( plane );
 }
 
 void Scene::render ( )
@@ -31,7 +33,14 @@ void Scene::render ( )
 	{
 		for ( int x = 0; x < m_pwindow->get_width ( ); ++x )
 		{
-			
+			//	Create a ray
+			Ray ray = m_pcamera->construct_ray ( x, y );
+
+			//	Trace the ray
+			ColourRGB pixel_colour = m_ptracer->trace_ray ( ray );
+
+			//	Set colour
+			m_pwindow->set_pixel ( x, y, pixel_colour );
 		}
 	}
 }
@@ -41,7 +50,17 @@ void Scene::update_window ( )
 	m_pwindow->update ( );
 }
 
-HitResult Scene::hit_objects ( const Ray& p_ray )
+void Scene::set_tracer ( IRayTracer* p_tracer )
 {
-	
+	m_ptracer = p_tracer;
+}
+
+void Scene::set_camera ( ICamera* p_camera )
+{
+	m_pcamera = p_camera;
+}
+
+const std::vector < IGeometry* >& Scene::get_geometry ( )
+{
+	return m_geometry;
 }
