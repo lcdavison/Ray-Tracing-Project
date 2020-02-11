f30121c7fb64f108dbdc20a00ab9ced664fb3f84
lcdavison <luke.davison@talktalk.net>
2020-02-11 19:04:45 +0000

    Fixed errors in ray construction

--- source/camera/pinhole_camera.cpp
+++ source/camera/pinhole_camera.cpp
@@ -1,5 +1,7 @@
 #include "camera/pinhole_camera.h"
 
+#include <iostream>
+
 PinholeCamera::PinholeCamera ( ) { }
 
 PinholeCamera::PinholeCamera ( const Point3& p_position, double p_vpdist ) : m_viewplane_dist ( p_vpdist )
@@ -20,11 +22,11 @@ void PinholeCamera::construct_basis ( const Point3& p_target, const Vector3& p_w
 Ray PinholeCamera::construct_ray ( double p_pixelx, double p_pixely )
 {
 	//	Remap coordinates
-	double x = p_pixelx - ( m_viewplane_width * 0.5 );	
-	double y = p_pixely - ( m_viewplane_height * 0.5 );
+	double x = p_pixelx - ( ( m_viewplane_width - 1 ) * 0.5 );	
+	double y = p_pixely - ( ( m_viewplane_height - 1 ) * 0.5 );
 
-	Vector3 ray_dir ( x - m_position.x, y - m_position.y, m_viewplane_dist );
-	ray_dir = ray_dir.x * m_basis [ 0 ] + ray_dir.y * m_basis [ 1 ] + ray_dir.z * m_basis [ 2 ];	//	Rotate ray into world space
+	Vector3 ray_dir ( x, y, -m_viewplane_dist );									//	Camera is origin of view space
+	ray_dir = ( ray_dir.x * m_basis [ 0 ] ) + ( ray_dir.y * m_basis [ 1 ] ) + ( ray_dir.z * m_basis [ 2 ] );	//	Transform ray into world space	- applies rotation
 	ray_dir.normalize ( );
 
 	return Ray ( m_position, ray_dir );
