0004663acc6f89fb91877652bbe33c0de55606b9
lcdavison <luke.davison@talktalk.net>
2020-02-11 19:06:03 +0000

    Fixed issues with vector length

--- source/maths/vector3.cpp
+++ source/maths/vector3.cpp
@@ -1,7 +1,7 @@
 #include "maths/vector3.h"
 
 Vector3::Vector3 ( ) : x ( 0.0 ), y ( 0.0 ), z ( 0.0 ) { }
-Vector3::Vector3 ( double p_x, double p_y, double p_z ) : x ( p_x ), y ( p_y ), z ( p_z ) { }
+Vector3::Vector3 ( double p_x, double p_y, double p_z ) : x ( p_x ), y ( p_y ), z ( p_z ), m_length ( std::sqrt ( x * x + y * y + z * z ) ) { }
 
 double Vector3::length ( )
 {
@@ -15,6 +15,8 @@ double Vector3::length ( )
 
 void Vector3::normalize ( )
 {
+	length ( );
+
 	x /= m_length;
 	y /= m_length;
 	z /= m_length;	
