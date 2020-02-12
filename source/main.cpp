57a0e6b1b2f9b3ab70ee514e4725176db2d88ad9
lcdavison <luke.davison@talktalk.net>
2020-02-12 17:24:12 +0000

    Added test materials and geometry

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,11 @@
 #include "camera/pinhole_camera.h"
 #include "raytracer/direct_tracer.h"
 
+#include "geometry/sphere.h"
+#include "geometry/plane.h"
+
+#include "material/matte.h"
+
 bool _running 	= false;
 bool _render 	= false;
 
@@ -13,7 +18,7 @@ int main ( int argc, char** argv )
 {
 	Scene scene ( 500, 500 );
 	
-	PinholeCamera* cam = new PinholeCamera ( Point3 ( 50.0, 0.0, 100.0 ), 100.0 );
+	PinholeCamera* cam = new PinholeCamera ( Point3 ( 0.0, 0.0, 100.0 ), 100.0 );
 	cam->construct_basis ( Point3 ( 0.0, 0.0, 0.0 ), Vector3 ( 0.0, 1.0, 0.0 ) );
 	cam->set_vpwidth ( 500 );
 	cam->set_vpheight ( 500 );
@@ -22,7 +27,19 @@ int main ( int argc, char** argv )
 	DirectTracer* tracey = new DirectTracer ( &scene );
 	scene.set_tracer ( tracey );
 
-	scene.build_scene ( );
+	Matte* s_mat = new Matte ( ColourRGB ( 0.0f, 0.0f, 0.5f ), 0.8f, 0.7f );
+
+	Sphere* sphere = new Sphere ( Point3 ( 0.0, 10.0, -40.0 ), 80.0 );
+	sphere->set_material ( s_mat );
+	scene.add_geometry ( sphere );
+
+	Matte* p_mat = new Matte ( ColourRGB ( 0.0f, 0.8f, 0.0f ), 0.8f, 0.7f );
+
+	Plane* plane = new Plane ( Point3 ( 0.0, 10.0, -100.0 ), Vector3 ( 0.0, 0.5, 0.5 ) );
+	plane->set_material ( p_mat );
+	scene.add_geometry ( plane );
+
+	scene.build_scene ( );	//	Change this function
 	scene.render ( );
 
 	_running = true;
