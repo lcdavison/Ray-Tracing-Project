ba2e77e94e6713949b45334f3a780396a378caf1
lcdavison <luke.davison@talktalk.net>
2020-02-12 15:22:22 +0000

    Added normal at hit point

--- include/geometry/hitresult.h
+++ include/geometry/hitresult.h
@@ -4,6 +4,9 @@
 #include <limits>
 
 #include "maths/maths.h"
+#include "material/material.h"
+
+class IMaterial;	//	Forward declaration
 
 /*
  *	HitResult contains all information about the intersection of
@@ -14,6 +17,7 @@ struct HitResult
 	bool 		m_hit = false;		//	Has ray hit something
 	double 		m_distance;		//	Distance of hit point
 	Point3		m_hitpoint;		//	Hit point in world coordinates
+	Vector3		m_normal;		//	Normal at hit point
 	IMaterial*	m_pmaterial;		//	Material of object
 };
 #endif
