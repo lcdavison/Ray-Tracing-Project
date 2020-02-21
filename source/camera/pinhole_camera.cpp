3d901b841b9a998adb8f0e32ff0e6f092c888082
lcdavison <luke.davison@talktalk.net>
2020-02-21 16:08:23 +0000

    Removed pixel center addition for later anti-aliasing support

--- source/camera/pinhole_camera.cpp
+++ source/camera/pinhole_camera.cpp
@@ -22,8 +22,8 @@ void PinholeCamera::construct_basis ( const Point3& p_target, const Vector3& p_w
 Ray PinholeCamera::construct_ray ( double p_pixelx, double p_pixely )
 {
 	//	Remap coordinates
-	double x = p_pixelx - ( ( m_viewplane_width - 1 ) * 0.5 ) + 0.5;	
-	double y = p_pixely - ( ( m_viewplane_height - 1 ) * 0.5 ) + 0.5;
+	double x = p_pixelx - ( ( m_viewplane_width - 1 ) * 0.5 );	
+	double y = p_pixely - ( ( m_viewplane_height - 1 ) * 0.5 );
 
 	Vector3 ray_dir ( x, y, -m_viewplane_dist );							//	Camera is origin of view space
 	ray_dir = ( ray_dir.x * m_basis [ 0 ] ) + ( ray_dir.y * m_basis [ 1 ] ) + ( ray_dir.z * m_basis [ 2 ] );	//	Transform ray into world space	- applies rotation
