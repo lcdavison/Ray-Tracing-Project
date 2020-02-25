9b0bf1698bca5f76cf44ac6b19f10dea20f3f225
lcdavison <luke.davison@talktalk.net>
2020-02-25 18:23:18 +0000

    Implemented area lighting

--- include/light/light.h
+++ include/light/light.h
@@ -17,11 +17,19 @@ class ILight
 
 		virtual Vector3 	get_direction 	( const Point3& ) = 0;
 
-		virtual bool		casts_shadows 	( ) { return m_shadows; }
-		virtual void		set_shadows 	( bool p_value ) { m_shadows = p_value; }
+		bool			casts_shadows 	( ) { return m_shadows; }
+		void			set_shadows 	( bool p_value ) { m_shadows = p_value; }
+
+		//	These are used for area lighting
+		virtual double 		geometric_factor ( ) 		 { return 1.0; }
+		virtual double		probability_density_function ( ) { return 1.0; }
+
+		virtual void		set_num_samples ( unsigned int p_num_samples ) { m_num_samples = p_num_samples; }
+		virtual unsigned int	get_num_samples ( ) { return m_num_samples; }
 
 	protected:
 
 		bool 			m_shadows = false;
+		unsigned int		m_num_samples = 1.0;
 };
 #endif
