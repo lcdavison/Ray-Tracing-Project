28e6d6ebf410b1a06293027160227ddbdf2a334c
lcdavison <luke.davison@talktalk.net>
2020-02-27 15:43:34 +0000

    Adjusted for compatibility with image writing

--- include/viewer/window.h
+++ include/viewer/window.h
@@ -12,8 +12,6 @@
 
 typedef std::chrono::time_point < std::chrono::high_resolution_clock > TimePoint;
 
-//	TODO: Use an SDL_Surface instead and manually blit the surface to the screen
-
 class Window 
 {
 	public:
@@ -25,11 +23,11 @@ class Window
 		void 					update ( );
 		void					present ( );
 
-		void 					set_pixel ( unsigned short, unsigned short, const ColourRGB& );
+		void 					set_pixel ( unsigned short, unsigned short, ColourRGB& );
 
 		unsigned short 				get_width ( );
 		unsigned short 				get_height ( );
-		const std::vector < ColourRGB >&	get_colourbuffer ( );
+		std::vector < ColourRGB >&		get_colourbuffer ( );
 
 	private:
 
@@ -37,13 +35,13 @@ class Window
 
 		void handle_events ( );
 
-		SDL_Window* 			m_pwindow = nullptr;
-		SDL_Surface*			m_surface_ptr = nullptr;
+		SDL_Window* 			m_window_ptr 	= nullptr;
+		SDL_Surface*			m_surface_ptr 	= nullptr;
 
 		std::vector < ColourRGB > 	m_colourbuffer;		//	Stores colours to later be written to an image file
 
 		//	Window dimensions : default 500w x 500h
-		unsigned short 			m_width = 500;
+		unsigned short 			m_width  = 500;
 		unsigned short 			m_height = 500;
 
 		TimePoint 			m_update_timer = std::chrono::high_resolution_clock::now ( );
