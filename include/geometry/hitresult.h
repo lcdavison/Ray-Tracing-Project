1d690ee96f277549e0470da897edb88adc2f3ced
lcdavison <luke.davison@talktalk.net>
2020-02-27 15:42:40 +0000

    Adjusted data returned from a ray hit

--- include/geometry/hitresult.h
+++ include/geometry/hitresult.h
@@ -26,11 +26,10 @@ struct HitResult
 	double 				m_distance;		//	Distance of hit point
 	Point3				m_hitpoint;		//	Hit point in world coordinates
 	Vector3				m_normal;		//	Normal at hit point
-	ColourRGB			m_reflection_radiance = ColourRGB ( 0.0f, 0.0f, 0.0f );	//	Radiance of reflections
-	Ray*				m_pray;			//	Ray which was traced
-	IMaterial*			m_material_ptr;		//	Material of object
-	ILight*				m_ambient_light_ptr;	//	Scene ambient light
-	std::vector < ILight* >*	m_lights_ptr;		//	Lights in scene
+	IMaterial*			m_material_ptr;		//	Material of hit object
+	ColourRGB			m_indirect_radiance;	//	Radiance recieved from other objects
+	ILight*				m_ambient_light_ptr;	//	Ambient light in scene
+	std::vector < ILight* >*	m_lights_ptr;		//	Lights in scene - used for shading
 	std::vector < IGeometry* >*	m_geometry_ptr;		//	Geometry in scene - used for shadows
 };
 #endif
