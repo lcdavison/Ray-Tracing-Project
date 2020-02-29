3835c0f781454b930cccd027679a937b26a4f054
lcdavison <luke.davison@talktalk.net>
2020-02-29 21:56:10 +0000

    Fixed incorrect generation of multi-jittered samples

--- source/sampling/multijittered_sampler.cpp
+++ source/sampling/multijittered_sampler.cpp
@@ -5,6 +5,9 @@ MultiJitteredSampler::MultiJitteredSampler ( unsigned int p_num_samples, unsigne
 	 m_num_samples	 = p_num_samples; 
 	 m_num_sets	 = p_num_sets;
 	 m_current_index = 0;
+
+	 std::random_device rd;
+	 m_generator 	 = std::default_random_engine ( rd ( ) );
 	 m_random 	 = std::uniform_int_distribution < unsigned int > ( 0, m_num_sets - 1 );
 
 	 setup_indices ( );
@@ -24,13 +27,14 @@ void MultiJitteredSampler::generate_samples ( )
 		{
 			for ( int x = 0; x < samples_per_dimension; ++x )
 			{
-				m_samples.emplace_back ( x / samples_per_dimension + y * subcell_width + rand ( m_generator ), y / samples_per_dimension + x * subcell_width + rand ( m_generator ), 0.0 );
+				m_samples.emplace_back ( ( ( double ) ( x ) / ( double ) ( samples_per_dimension ) ) + ( y * subcell_width ) + rand ( m_generator ), 
+							 ( ( double ) ( y ) / ( double ) ( samples_per_dimension ) ) + ( x * subcell_width ) + rand ( m_generator ), 0.0 );
 			}
 		}
 	}
 
-	std::mt19937 mt;
 	std::uniform_int_distribution < unsigned int > shuffle ( 0, samples_per_dimension - 1 );
+	m_generator.seed ( std::time ( nullptr ) );
 
 	//	Shuffle X coordinates
 	for ( int s = 0; s < m_num_sets; ++s )
@@ -39,7 +43,7 @@ void MultiJitteredSampler::generate_samples ( )
 		{
 			for ( int x = 0; x < samples_per_dimension; ++x )
 			{
-				unsigned int index = x + ( shuffle ( mt ) % ( samples_per_dimension - x ) );
+				unsigned int index = x + ( shuffle ( m_generator ) % ( samples_per_dimension - x ) );
 
 				double temp = m_samples.at ( s * m_num_samples + y * samples_per_dimension + x ).x;
 				m_samples.at ( s * m_num_samples + y * samples_per_dimension + x ).x = m_samples.at ( s * m_num_samples + y * samples_per_dimension + index ).x;
@@ -49,6 +53,7 @@ void MultiJitteredSampler::generate_samples ( )
 	}
 
 	shuffle.reset ( );
+	m_generator.seed ( std::time ( nullptr ) );
 	
 	//	Shuffle Y coordinates
 	for ( int s = 0; s < m_num_sets; ++s )
@@ -57,7 +62,7 @@ void MultiJitteredSampler::generate_samples ( )
 		{
 			for ( int x = 0; x < samples_per_dimension; ++x )
 			{
-				unsigned int index = y + ( shuffle ( mt ) % ( samples_per_dimension - y ) );
+				unsigned int index = y + ( shuffle ( m_generator ) % ( samples_per_dimension - y ) );
 
 				double temp = m_samples.at ( s * m_num_samples + y * samples_per_dimension + x ).y;
 				m_samples.at ( s * m_num_samples + y * samples_per_dimension + x ).y = m_samples.at ( s * m_num_samples + index * samples_per_dimension + x ).y;
